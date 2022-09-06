9c9fe362a1d93165616dafbabd88e86f318cc004
Jason <jayaries329@yahoo.com>
2022-09-06 15:31:44 -0700

    Incomplete week-5 assessment. Need to do JS challenges and Jest tests.

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -9,6 +9,10 @@ letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
+# def finder (array, thing_to_find)
+#     array.select {|word| word.include? thing_to_find}
+# end
+# p finder(beverages_array, letter_t)
 
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
@@ -16,13 +20,29 @@ letter_t = 't'
 
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Vermont'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Vermont', 'Washington'] 
+# def in_order hash
+#     hash.values.flatten.sort
+# end
+# p in_order(us_states)
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+class Bike
+    attr_accessor :model, :wheels, :current_speed
+    def initialize(name)
+        @model = 'Trek'
+        @wheels = 2
+        @current_speed = 0
+    end
+    def get_info
+        "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+end
 
-
+bike1 = Bike.new('Giant')
+p bike1.get_info
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
