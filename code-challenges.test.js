a9d1a944e0a645a610b2d3f34120d48d3f12c09f
Jason <jayaries329@yahoo.com>
2022-09-08 00:42:55 -0700

    This is completed week 5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -21,11 +21,75 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+describe("messageCoder", () => {
+    test ("returns a coded message", () => {
+        expect (messageCoder(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+        expect (messageCoder(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        expect (messageCoder(secretCodeWord3)).toEqual("3cc3ntr1c")
+        
+    })
+})
+// Good Failure!!!
+// FAIL  ./code-challenges.test.js
+// messageCoder
+//   ✕ returns a coded message (2 ms)
+
+// ● messageCoder › returns a coded message
+
+//   ReferenceError: messageCoder is not defined
+
+//     24 | describe("messageCoder", () => {
+//     25 |     test ("returns a coded message", () => {
+//   > 26 |         expect (messageCoder(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+//        |         ^
+//     27 |         expect (messageCoder(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+//     28 |         expect (messageCoder(secretCodeWord3)).toEqual("3cc3ntr1c")
+//     29 |         
+
+//     at Object.expect (code-challenges.test.js:26:9)
+
+// Test Suites: 1 failed, 1 total
+
+
+
 // b) Create the function that makes the test pass.
+// Pseudo code:
+// Create a function named messageCoder that takes in a string
+// Use a combination of built-in methods .map, .split, .join, and a conditional statement for iteration and conversions
+// Return a coded message
+
+const messageCoder = (string) => {
+    arrayMorph = string.split("")
+    const vowelConvert = arrayMorph.map(value => {
+        if (value === 'a' || value === 'A') {
+            return '4';
+        } if (value === 'e' || value === 'E') {
+            return '3'
+        } if (value === 'i' || value === 'I') {
+            return '1'
+        } if (value === 'o' || value === 'O') {
+            return '0'
+        } else
+            return value
+      });
+    return vowelConvert.join('')
+}
+// Test passed!!!
+// PASS  ./code-challenges.test.js
+// messageCoder
+//   ✓ returns a coded message (3 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.57 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 1.82s.
+
+
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
-
-// a) Create a test with expects statement using the variable provided.
 
 const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
@@ -34,10 +98,58 @@ const letterA = "a"
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+// a) Create a test with expects statement using the variable provided.
+describe("fruitByLetter", () => {
+    test ("returns an array of all the words containing that particular letter", () => {
+        expect (fruitByLetter(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect (fruitByLetter(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+        
+        
+    })
+})
+// Good Failure!!!
+// FAIL  ./code-challenges.test.js
+// fruitByLetter
+//   ✕ returns an array of all the words containing that particular letter (1 ms)
+
+// ● fruitByLetter › returns an array of all the words containing that particular letter
+
+//   ReferenceError: fruitByLetter is not defined
+
+//     102 | describe("fruitByLetter", () => {
+//     103 |     test ("returns an array of all the words containing that particular letter", () => {
+//   > 104 |         expect (fruitByLetter(letterA)).toEqual(["Mango", "Apricot", "Peach"])
+//         |         ^
+//     105 |         expect (fruitByLetter(letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+//     106 |         
+//     107 |         
+
+//     at Object.expect (code-challenges.test.js:104:9)
+
+
 // b) Create the function that makes the test pass.
+// Pseudo code:
+// Create a function named fruitByLetter that takes in an array and a string
+// Use a combination of built-in methods .filter, .includes, and .toLowerCase in a high-order function for iteration and element selection based on letter
+// Return edited, shorter array
+const fruitByLetter = (array, str) => {
+    return array.filter((array) => array.toLowerCase().includes(str))
+  }
+//   Test Passed!!!
+//   PASS  ./code-challenges.test.js
+//   fruitByLetter
+//     ✓ returns an array of all the words containing that particular letter (2 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.694 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 1.81s.
+
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
-
+// *** I powered my way through the previous 2 challenges and getting those tests to pass, so I threw in the towel on this last one in an effort to at least submit something. ***
 // a) Create a test with expect statements using the variable provided.
 
 const hand1 = [5, 5, 5, 3, 3]
