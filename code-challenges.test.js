69598550e10e50ebb2350be4b39a432031c5ec9c
Jason <jayaries329@yahoo.com>
2022-09-09 16:43:21 -0700

    This is another submission.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,12 +14,12 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+// const secretCodeWord1 = "Lackadaisical"
+// // Expected output: "L4ck4d41s1c4l"
+// const secretCodeWord2 = "Gobbledygook"
+// // Expected output: "G0bbl3dyg00k"
+// const secretCodeWord3 = "Eccentric"
+// // Expected output: "3cc3ntr1c"
 
 describe("messageCoder", () => {
     test ("returns a coded message", () => {
@@ -74,64 +74,64 @@ const messageCoder = (string) => {
       });
     return vowelConvert.join('')
 }
-// Test passed!!!
-// PASS  ./code-challenges.test.js
-// messageCoder
-//   ✓ returns a coded message (3 ms)
+// // Test passed!!!
+// // PASS  ./code-challenges.test.js
+// // messageCoder
+// //   ✓ returns a coded message (3 ms)
 
-// Test Suites: 1 passed, 1 total
-// Tests:       1 passed, 1 total
-// Snapshots:   0 total
-// Time:        0.57 s, estimated 1 s
-// Ran all test suites.
-// ✨  Done in 1.82s.
-
-
+// // Test Suites: 1 passed, 1 total
+// // Tests:       1 passed, 1 total
+// // Snapshots:   0 total
+// // Time:        0.57 s, estimated 1 s
+// // Ran all test suites.
+// // ✨  Done in 1.82s.
 
 
-// --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+// // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
-// a) Create a test with expects statement using the variable provided.
+// const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+
+// const letterA = "a"
+// // Expected output: ["Mango", "Apricot", "Peach"]
+// const letterE = "e"
+// // Expected output: ["Cherry", "Blueberry", "Peach"]
+
+// // a) Create a test with expects statement using the variable provided.
 describe("fruitByLetter", () => {
     test ("returns an array of all the words containing that particular letter", () => {
         expect (fruitByLetter(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
         expect (fruitByLetter(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
         
         
-    })
-})
-// Good Failure!!!
-// FAIL  ./code-challenges.test.js
-// fruitByLetter
-//   ✕ returns an array of all the words containing that particular letter (1 ms)
+//     })
+// })
+// // Good Failure!!!
+// // FAIL  ./code-challenges.test.js
+// // fruitByLetter
+// //   ✕ returns an array of all the words containing that particular letter (1 ms)
 
-// ● fruitByLetter › returns an array of all the words containing that particular letter
+// // ● fruitByLetter › returns an array of all the words containing that particular letter
 
-//   ReferenceError: fruitByLetter is not defined
+// //   ReferenceError: fruitByLetter is not defined
 
-//     102 | describe("fruitByLetter", () => {
-//     103 |     test ("returns an array of all the words containing that particular letter", () => {
-//   > 104 |         expect (fruitByLetter(letterA)).toEqual(["Mango", "Apricot", "Peach"])
-//         |         ^
-//     105 |         expect (fruitByLetter(letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
-//     106 |         
-//     107 |         
+// //     102 | describe("fruitByLetter", () => {
+// //     103 |     test ("returns an array of all the words containing that particular letter", () => {
+// //   > 104 |         expect (fruitByLetter(letterA)).toEqual(["Mango", "Apricot", "Peach"])
+// //         |         ^
+// //     105 |         expect (fruitByLetter(letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+// //     106 |         
+// //     107 |         
 
-//     at Object.expect (code-challenges.test.js:104:9)
+// //     at Object.expect (code-challenges.test.js:104:9)
 
 
-// b) Create the function that makes the test pass.
-// Pseudo code:
-// Create a function named fruitByLetter that takes in an array and a string
-// Use a combination of built-in methods .filter, .includes, and .toLowerCase in a high-order function for iteration and element selection based on letter
-// Return edited, shorter array
+// // b) Create the function that makes the test pass.
+// // Pseudo code:
+// // Create a function named fruitByLetter that takes in an array and a string
+// // Use a combination of built-in methods .filter, .includes, and .toLowerCase in a high-order function for iteration and element selection based on letter
+// // Return edited, shorter array
 const fruitByLetter = (array, str) => {
     return array.filter((array) => array.toLowerCase().includes(str))
   }
@@ -149,8 +149,6 @@ const fruitByLetter = (array, str) => {
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
-// *** I powered my way through the previous 2 challenges and getting those tests to pass, so I threw in the towel on this last one in an effort to at least submit something. ***
-// a) Create a test with expect statements using the variable provided.
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -161,4 +159,65 @@ const hand3 = [5, 5, 5, 5, 4]
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
+// a) Create a test with expect statements using the variable provided.
+describe("houseOfCards", () => {
+    it("takes in an array of numbers and determines whether its a full house", () => {
+        expect(houseOfCards(hand1)).toEqual(true)
+        expect(houseOfCards(hand2)).toEqual(false)
+        expect(houseOfCards(hand3)).toEqual(false)
+        expect(houseOfCards(hand4)).toEqual(true)
+    })
+})
+// Good Failure!!!
+// FAIL  ./code-challenges.test.js
+// houseOfCards
+//   ✕ takes in an array of numbers and determines whether its a full house (1 ms)
+
+// ● houseOfCards › takes in an array of numbers and determines whether its a full house
+
+//   ReferenceError: houseOfCards is not defined
+
+//     163 | describe("houseOfCards", () => {
+//     164 |     it("takes in an array of numbers and determines whether its a full house", () => {
+//   > 165 |         expect(houseOfCards(hand1)).toEqual(true)
+//         |         ^
+//     166 |         expect(houseOfCards(hand2)).toEqual(false)
+//     167 |         expect(houseOfCards(hand3)).toEqual(false)
+//     168 |         expect(houseOfCards(hand4)).toEqual(true)
+
+//     at Object.expect (code-challenges.test.js:165:9)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// Snapshots:   0 total
+// Time:        0.751 s, estimated 1 s
+
+
 // b) Create the function that makes the test pass.
+// Pseudo code:
+// Create a function called houseOfCards that takes in an array of numbers
+// Use a built-in method .sort in conjunction with a for loop to iterate through and compare numbers for matches
+// Return boolean values (true/false) as results of the number comparisons in the iteration.
+const houseOfCards = ( array ) => {
+    let cardChecker = array.sort()
+    for (let i = 0;i < cardChecker.length; i++){
+        if(cardChecker[0] === cardChecker[1] && cardChecker[1] === cardChecker[2] && cardChecker[3] === cardChecker[4]){
+            return true
+        }else if(cardChecker[0] === cardChecker[1] && cardChecker[2] === cardChecker[3] && cardChecker[2] === cardChecker[4]){
+            return true
+        }else 
+        return false
+    }
+}
+
+// Test Passed!!!
+// PASS  ./code-challenges.test.js
+// houseOfCards
+//   ✓ takes in an array of numbers and determines whether its a full house (2 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// Time:        0.638 s, estimated 1 s
+// Ran all test suites.
+// ✨  Done in 1.74s.
